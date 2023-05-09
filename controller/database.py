76015684cbae590c3331950a60c2a974eb91e2ad
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-05-10 00:03:56 +0900

    .

--- controller/database.py
+++ controller/database.py
@@ -1,28 +1,32 @@
-from pymysqlpool.pool import Pool
+import pymysqlpool
+from pymysql import cursors
+pymysqlpool.logger.setLevel('DEBUG')
+config ={'host':'database-1.cedn2xc6oolp.ap-northeast-2.rds.amazonaws.com','port':3306,'user':'ruddls030','password':'dlstn0722!','db':'DELIENG','autocommit':True}
 
 def __init__():
-    global pool
-    pool = Pool(host='database-1.cedn2xc6oolp.ap-northeast-2.rds.amazonaws.com',port=3306,user='ruddls030',password='dlstn0722!',db='food',autocommit=True)
-    #MySQL 데이터베이스를 연결하고 변경사항이 생길때 자동으로 커밋하게 한다. 사용했던 회선을 재 사용함으로서 메모리 낭비가 발생하지 않도록 한다.
-    pool.init()
-    print('DB connection established')
+    global pool1
+    pool1 = pymysqlpool.ConnectionPool(size=20, maxsize= 100, pre_create_num=20, name='pool1', **config)
 
-def execute_sql(qurey: str):
-    connection = pool.get_conn()
-    cur = connection.cursor()
-    def get(qurey):
-        cur.execute(qurey)
-        return cur.fetchall()
-    
-    def edit(qurey):
-        res = cur.execute(qurey)
+def execute_sql(sql:str):
+    global pool1
+    con = pool1.get_connection().cursor(cursors.DictCursor)
+    with con as cur:
+
+        def get(sql):
+            cur.execute(sql)
+            return cur.fetchall()
+        
+        def edit(sql):
+            res = cur.execute(sql)
+            return res
+
+        if "SELECT" in sql or "select" in sql:
+            res = get(sql)
+        else:
+            res = edit(sql)
+
+        con.close()
+        
+        print(res)
+
         return res
-
-    if "SELECT" in qurey or "select" in qurey:
-        res = get(qurey)
-    else:
-        res = edit(qurey)
-    
-    pool.release(connection)
-
-    return res
