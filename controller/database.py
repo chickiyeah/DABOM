f165810d7f272e1567a8803ca0e5203ca08913bc
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-05-10 20:32:00 +0900

    Update database.py

--- controller/database.py
+++ controller/database.py
@@ -1,32 +1,37 @@
 import pymysqlpool
 from pymysql import cursors
 pymysqlpool.logger.setLevel('DEBUG')
-config ={'host':'database-1.cedn2xc6oolp.ap-northeast-2.rds.amazonaws.com','port':3306,'user':'ruddls030','password':'dlstn0722!','db':'DELIENG','autocommit':True}
+config ={'host':'database-1.cedn2xc6oolp.ap-northeast-2.rds.amazonaws.com','port':3306,'user':'ruddls030','password':'dlstn0722!','db':'food','autocommit':True}
 
 def __init__():
     global pool1
-    pool1 = pymysqlpool.ConnectionPool(size=20, maxsize= 100, pre_create_num=20, name='pool1', **config)
+    pool1 = pymysqlpool.ConnectionPool(size=2, pre_create_num=2, name='pool1', **config)
 
 def execute_sql(sql:str):
     global pool1
-    con = pool1.get_connection().cursor(cursors.DictCursor)
-    with con as cur:
+    con = pool1.get_connection()
+    cursor = con.cursor(cursors.DictCursor)
+    with cursor as cur:
 
         def get(sql):
             cur.execute(sql)
+            try:
+                con.close()
+            except pymysqlpool.ReturnConnectionToPoolError:
+                "no"
+
             return cur.fetchall()
         
         def edit(sql):
             res = cur.execute(sql)
+            try:
+                con.close()
+            except pymysqlpool.ReturnConnectionToPoolError:
+                "no"
+                
             return res
 
         if "SELECT" in sql or "select" in sql:
-            res = get(sql)
+            get(sql)
         else:
-            res = edit(sql)
-
-        con.close()
-        
-        print(res)
-
-        return res
+            edit(sql)
