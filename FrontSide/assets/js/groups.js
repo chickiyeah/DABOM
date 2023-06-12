8788154388c9a0ee6e24b39f8d7bde44b4da35e8
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-06-12 20:44:04 +0900

    Update groups.js

--- FrontSide/assets/js/groups.js
+++ FrontSide/assets/js/groups.js
@@ -92,6 +92,44 @@ clickEnter(element_invite_group_email_input, element_invite_group_button);
 
 var joined
 
+function button_Event(pointerevent) {
+    let target = pointerevent.target
+    let type = target.type
+    let id = target.id
+    let name = target.parentElement.children[0].innerText
+
+    if (type === "register") {
+        let reg_confirm = confirm(`정말 ${name} 모임에 가입하시겠습니까?`)
+    }
+
+    if (type === "unregister") {
+        let unreg_confirm = confirm(`정말 ${name} 모임에서 탈퇴하시겠습니까?`)
+    }
+
+    if (type === "delete") {
+        let name = target.parentElement.parentElement.children[0].innerText
+        let delete_confirm = confirm(`정말 ${name} 모임을 삭제하시겠습니까?\n이 작업은 취소할수 없습니다!`)
+    }
+
+}
+
+
+function apply_event(div) {
+    Array.prototype.forEach.call(div.children,(element) =>{
+        element.children[1].children[2].removeEventListener("click", button_Event);
+        element.children[1].children[2].addEventListener("click", button_Event);
+    })
+}
+
+function apply_edit_event() {
+    Array.prototype.forEach.call(onwed_ul.children,(element) =>{
+        element.children[1].children[2].children[0].removeEventListener("click", button_Event);
+        element.children[1].children[2].children[1].removeEventListener("click", button_Event)
+        element.children[1].children[2].children[0].addEventListener("click", button_Event);
+        element.children[1].children[2].children[1].addEventListener("click", button_Event);
+    })
+}
+
 async function list_public(page) {
     if (page <= 0) {
         return new Error("wrong page")
@@ -157,6 +195,7 @@ async function list_public(page) {
                             let mem = JSON.parse(group.members).length
                             let name = group.name
                             let img = group.groupimg
+                            let no = group.no
                             if (joined.includes(group.no)){
                                 html = `<li>
                                             <div class="img_box">
@@ -165,7 +204,7 @@ async function list_public(page) {
                                             <div class="info_box">
                                                 <p class="title">${name}</p>
                                                 <p class="sub_txt">맴버: ${mem}명</p>
-                                                <a href="javascript:">상세보기</a>
+                                                <a id="${no}" type="register" href="javascript:">상세보기</a>
                                             </div>
                                         </li>`
                             } else {
@@ -176,13 +215,14 @@ async function list_public(page) {
                                             <div class="info_box">
                                                 <p class="title">${name}</p>
                                                 <p class="sub_txt">맴버: ${mem}명</p>
-                                                <a href="javascript:">가입하기</a>
+                                                <a id="${no}" type="register" href="javascript:">가입하기</a>
                                             </div>
                                         </li>`
                             }
 
                             public_ul.insertAdjacentHTML("beforeend", html)
                         })
+                        apply_event(public_ul)
                         document.querySelector(".loading").style.display = "none"
                     })
                 })
@@ -246,6 +286,7 @@ async function mygroups(page) {
                     let mem = JSON.parse(group.members).length
                     let name = group.name
                     let img = group.groupimg
+                    let no = group.no
                     let html = `<li>
                                     <div class="img_box">
                                         <img alt="게시글 이미지" src="${img}">
@@ -253,13 +294,14 @@ async function mygroups(page) {
                                     <div class="info_box">
                                         <p class="title">${name}</p>
                                         <p class="sub_txt">맴버: ${mem}명</p>
-                                        <a class="black_btn" href="javascript:">탈퇴하기</a>
+                                        <a id="${no}" type="unregister" class="black_btn" href="javascript:">탈퇴하기</a>
                                     </div>
                                 </li>`
                     
                                 
                     joined_ul.insertAdjacentHTML("beforeend", html)
                 })
+                apply_event(joined_ul)
                 document.querySelector(".loading").style.display = "none"
             })
         })
@@ -321,6 +363,7 @@ async function list_owned(page) {
                     let mem = JSON.parse(group.members).length
                     let name = group.name
                     let img = group.groupimg
+                    let no = group.no
                     let html = `<li>
                                     <div class="img_box">
                                         <img alt="게시글 이미지" src="${img}">
@@ -329,8 +372,8 @@ async function list_owned(page) {
                                         <p class="title">${name}</p>
                                         <p class="sub_txt">맴버: ${mem}명</p>
                                         <div class="btn_box">
-                                            <a class="gray_btn" href="javascript:">수정</a>
-                                            <a class="black_btn" href="javascript:">삭제</a>
+                                            <a id="${no}" type="edit" class="gray_btn" href="javascript:">수정</a>
+                                            <a id="${no}" type="delete" class="black_btn" href="javascript:">삭제</a>
                                         </div>
                                     </div>
                                 </li>`
@@ -338,7 +381,9 @@ async function list_owned(page) {
 
                     onwed_ul.insertAdjacentHTML('beforeend', html)
                 })
+                apply_edit_event()
                 document.querySelector(".loading").style.display = "none"
+                
             })
         })
     }
