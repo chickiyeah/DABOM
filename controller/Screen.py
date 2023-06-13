dd8ca7525fdc047d132f95cf7b7407cd9baded1a
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-06-13 20:04:05 +0900

    13233132
    Co-Authored-By: Yurim <74886033+yurim@users.noreply.github.com>

--- controller/Screen.py
+++ controller/Screen.py
@@ -62,6 +62,10 @@ async def groups(request: Request):
 async def group_detail(request: Request):
     return templates.TemplateResponse("group_list.html", {"request":request})
 
+@ScreenRoute.get("/group/add")
+async def group_add(request: Request):
+    return templates.TemplateResponse("group_add.html", {"request":request}) 
+
 @ScreenRoute.get("/friend/request/{request_id}/{request_nickname}/{target_id}/{target_nickname}/{verify_id}")
 async def friend_accept_reject(request: Request):
     return templates.TemplateResponse("email_friend_req.html", {"request":request})
